7f707adeec389d6a87bbad15d228f7002ae0b4e5
Mohd Ahsan <ahsanshamsisa@gmail.com>
2025-05-14 13:11:58 +0530

    update logger for otel

--- src/config/otelConfig.ts
+++ src/config/otelConfig.ts
@@ -1,6 +1,6 @@
 'use strict';
 require('dotenv').config();
-import logger from "../utils/logger";
+import { logger } from "../utils/logger";
 logger.info("Starting opentelemetry tracing for automation-api-service");
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
