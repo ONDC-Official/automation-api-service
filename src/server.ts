7f6f94e582a1ef49d0c6406216a66d4b98797e05
Mohd Ahsan <ahsanshamsisa@gmail.com>
2025-05-20 00:31:45 +0530

    fix - tracing deprecated warning

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-require("./config/otelConfig")
+import "./config/otelConfig"
 import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { logError, logger } from "./utils/logger";
