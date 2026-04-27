2d53b578f361860861c4ba31e7140efea8f0b1d8
Amit Kumar <amit.kumar@ondc.org>
2026-04-27 15:43:01 +0530

    Fix logging of Workbench unique key id

--- src/utils/headerUtils.ts
+++ src/utils/headerUtils.ts
@@ -12,7 +12,7 @@ const createAuthHeader = async (
 	try {
 		logger.info("Creating Authorization Header", loggerMeta);
 		logger.info("Payload for auth header ", payload);
-		logger.info("Workbench unique key id ": , process.env.UKID);
+		logger.info("Workbench unique key id ", process.env.UKID);
 	
 		const subId =
 			env === "LOGGED-IN"
